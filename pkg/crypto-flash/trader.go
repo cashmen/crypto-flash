81f1fd5bb7f9e91df58f3fe1078e2ed9d110194e
CheshireCatNick <CheshireCatNick@gmail.com>
2020-03-09 00:32:53 +0800

    trader from trader pkg to crypto flash pkg

--- pkg/crypto-flash/trader.go
+++ pkg/crypto-flash/trader.go
@@ -2,15 +2,18 @@ package cryptoflash
 
 import "fmt"
 
+// Trader represent a trader in crypto flash
 type Trader struct {
 	id string
 }
 
+// NewTrader creates a trader instance
 func NewTrader() *Trader {
 
 	return &Trader{id: "1234"}
 }
 
+// Run starts a trader
 func (t *Trader) Run() {
 	fmt.Println("start trading")
 }
