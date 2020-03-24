2e4f823f3335d8c394fc762fbe32ee5766dbf2eb
CheshireCatNick <CheshireCatNick@gmail.com>
2020-03-24 23:10:10 +0800

    rename super trend to supertrend

---
+++ pkg/indicator/supertrend.go
@@ -0,0 +1,86 @@
+package indicator
+
+import util "github.com/CheshireCatNick/crypto-flash/pkg/util"
+
+type Supertrend struct {
+	period int
+	multiplier float64
+	atr *ATR
+	prevFinalUpperBand float64
+	prevFinalLowerBand float64
+	prevTrend string
+	prevCandle *util.Candle
+}
+
+const tag = "Supertrend"
+func NewSupertrend(multiplier float64, period int) *Supertrend {
+	return &Supertrend{
+		period: period,
+		multiplier: multiplier,
+		atr: NewATR(period),
+		prevCandle: nil,
+	}
+}
+func (st *Supertrend) CalculateSupertrend(candles []*util.Candle) []float64 {
+	tst := NewSupertrend(st.multiplier, st.period)
+	result := []float64{}
+	for _, candle := range candles {
+		result = append(result, tst.Update(candle))
+	}
+	return result;
+}
+func (st *Supertrend) Update(candle *util.Candle) float64 {
+	atr := st.atr.Update(candle)
+	basicUpperBand := candle.GetAvg() + st.multiplier * atr
+	basicLowerBand := candle.GetAvg() - st.multiplier * atr
+	var finalUpperBand, finalLowerBand, supertrend float64
+	if st.prevCandle == nil {
+		finalUpperBand = basicUpperBand
+	} else if basicUpperBand < st.prevFinalUpperBand || 
+		st.prevCandle.Close > st.prevFinalUpperBand {
+		// price is falling or in up trend, adjust upperband
+		finalUpperBand = basicUpperBand
+	} else {
+		// price is rising, maintain upperband
+		finalUpperBand = st.prevFinalUpperBand
+	}
+	if st.prevCandle == nil {
+		finalLowerBand = basicLowerBand
+	} else if basicLowerBand > st.prevFinalLowerBand ||
+		st.prevCandle.Close < st.prevFinalLowerBand {
+		// price is rising or in down trend, adjust lowerband
+		finalLowerBand = basicLowerBand
+	} else {
+		// price is falling, maintain lowerband
+		finalLowerBand = st.prevFinalLowerBand
+	}
+	/* another version
+	if candles[i].Close <= finalUpperBand {
+		superTrend = finalUpperBand
+	} else {
+		superTrend = finalLowerBand
+	}*/
+	if candle.Close >= finalUpperBand {
+		util.Info(tag, util.Green("trend up"))
+		supertrend = finalLowerBand
+		st.prevTrend = "up"
+	} else if candle.Close <= finalLowerBand {
+		util.Info(tag, util.Red("trend down"))
+		supertrend = finalUpperBand
+		st.prevTrend = "down"
+	} else {
+		// final lower band < close < final upper band
+		// keep previous trend
+		if (st.prevTrend == "up") {
+			supertrend = finalLowerBand
+		} else if (st.prevTrend == "down") {
+			supertrend = finalUpperBand
+		} else {
+			supertrend = -1
+		}
+	}
+	st.prevFinalUpperBand = finalUpperBand
+	st.prevFinalLowerBand = finalLowerBand
+	st.prevCandle = candle
+	return supertrend
+}
