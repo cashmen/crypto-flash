aa95e0c95d649bb54a028316a707a642eab057e2
cashmen <cashmen@gmx.net>
2020-05-27 10:53:34 +0200

    Update res-trend.go

--- pkg/character/res-trend.go
+++ pkg/character/res-trend.go
@@ -63,7 +63,7 @@ func NewResTrend(ftx *exchange.FTX, notifier *Notifier) *ResTrend {
 		period: 3,
 		warmUpCandleNum: 40,
 		takeProfit: 100,
-		stopLoss: 150,
+		stopLoss: 100,
 		useTrailingStop: false,
 		// data
 		mainCandle: nil,
