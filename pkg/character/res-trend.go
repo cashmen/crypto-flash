847ccd2ea1413e05228b57297c85b27195848e08
cashmen <cashmen@gmx.net>
2020-12-08 15:12:56 +0100

    Update res-trend.go

--- pkg/character/res-trend.go
+++ pkg/character/res-trend.go
@@ -57,13 +57,13 @@ func NewResTrend(ftx *exchange.FTX, notifier *Notifier) *ResTrend {
 		// config
 		market: "BTC-PERP",
 		mul: 1,
-		res: 15, // 15 (for test), 60, 300 or 900
+		res: 900, // 15 (for test), 60, 300 or 900
 		mainMul: 2,
-		mainRes: 15, // 60 (for test), 3600 or 14400
+		mainRes: 14400, // 60 (for test), 3600 or 14400
 		period: 3,
 		warmUpCandleNum: 50,
-		takeProfit: 3,
-		stopLoss: 3,
+		takeProfit: 100,
+		stopLoss: 100,
 		useTrailingStop: true,
 		// data
 		mainCandle: nil,
